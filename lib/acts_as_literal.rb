ea720c009935acea65a9d7881beb6f5d1c7b64cf
kp <kakkunpakkun@gmail.com>
2011-10-31 13:05:01 +0900

    fixed syntax error.

--- lib/acts_as_literal.rb
+++ lib/acts_as_literal.rb
@@ -2,10 +2,12 @@ require "acts_as_literal/version"
 
 module ActsAsLiteral
     def self.included(base)
-    base.instance_eval do
-    def acts_as_literal(column_name = "name")
-        define_method(:to_param) do
-            "#{id}-#{URI.encode(self[column_name].to_s)}"
-        end
-    end
+	    base.instance_eval do
+		    def acts_as_literal(column_name = "name")
+		        define_method(:to_param) do
+		            "#{id}-#{URI.encode(self[column_name].to_s)}"
+		        end
+		    end
+		end
+	end
 end
